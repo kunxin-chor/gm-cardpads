b40f467fc16f33bc472db96428d0603a87c1eecd
kunxin-chor <chorkunxin@yahoo.com>
2020-04-22 15:19:21 +0000

    Run tests on local mongo instead

--- gm_card_app_backend/data.py
+++ gm_card_app_backend/data.py
@@ -1,13 +1,16 @@
 import pymongo
 import os
 
-DB_URL = os.environ.get('DB_URL')
+DB_URL = os.environ.get('MONGO_URI')
 DATABASE_NAME = "cardpad"
 
 def create_connection(db_url=""):
     if len(db_url) == 0:
         db_url = DB_URL
-    return pymongo.MongoClient(db_url)
+    return pymongo.MongoClient(db_url) 
+
+def get_database(db_url=""):
+    return create_connection()[DATABASE_NAME]
 
 def add_card(user_id, title, content, tags):
     conn = create_connection()
@@ -19,8 +22,9 @@ def add_card(user_id, title, content, tags):
     })
     return insert_result.inserted_id
 
+
 def find_card_by_tag(user_id, tag):
-    conn = create_connection
+    conn = create_connection()
     cards = conn[DATABASE_NAME]["cards"].find({
         "user_id":user_id,
         "tags":tag
--- gm_card_app_backend/tests/test_cards.py
+++ gm_card_app_backend/tests/test_cards.py
@@ -1,14 +1,17 @@
+from dotenv import load_dotenv
 from app import app
 from bson.objectid import ObjectId
 import json
 import data
 
+data.DB_URL="mongodb://localhost:27017"
+
+
 def setup_function():
     data.DATABASE_NAME = "cardpad_test"
-
-def teardown_function():
     with data.create_connection() as conn:
-        conn.drop_database('cardpad_test')
+        conn[data.DATABASE_NAME]['cards'].delete_many({})
+
 
 def test_add_card():
     response = app.test_client().post('/add-card',json={
@@ -19,16 +22,25 @@ def test_add_card():
     assert response.status_code == 200
 
     response_data = response.get_json()
-    print(response_data)
-
+    
 
     # test the db
     with data.create_connection() as conn:
         found = conn['cardpad_test']['cards'].find_one({
             '_id':ObjectId(response_data['new_card_id']['$oid'])
         })
-        print(found)
         assert found != None
         assert found['title'] == 'Hello world'
         assert found['content'] == 'This is a good day to live'
         assert found['tags'] == 'a,b,c'
+
+def test_find_card():
+    fake_user_id = ObjectId()
+    data.add_card(fake_user_id, "Mary has a little lamb", "ASD", ['a','b'])
+    data.add_card(fake_user_id, "Mary has a little lamb", "ASD", ['a'])
+
+    cards = data.find_card_by_tag(fake_user_id, 'a')
+    assert len(list(cards)) == 2
+
+    cards = data.find_card_by_tag(fake_user_id, 'b')
+    assert len(list(cards)) == 1
